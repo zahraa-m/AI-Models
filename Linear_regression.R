265330a70148dddbee40bb8840b17a83fc4e75c0
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-22 23:55:04 +0100

    Add files via upload

---
+++ Linear_regression.R
@@ -0,0 +1,17 @@
+#simple linear regression from scratch 
+x <- c(1, 2, 4)
+y <- c(2, 1, 3)
+
+#find the regression line y=mx+b
+# 1- calculate m from data above (x,y)
+ex_x <- sum(x) / length(x)
+ex_y <- sum(y) / length(y)
+sq_ex_x <- sum(x^2) / length(x)
+ex_xy <- sum(x*y) / length(x)
+m <- (ex_x * ex_y - ex_xy) / ((ex_x)^2 - sq_ex_x)
+
+# 2- calculate b 
+b <- ex_y - m * ex_x
+
+#print the regression line:
+print("y= %s + %s * x", b, m)
