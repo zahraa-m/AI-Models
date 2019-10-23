e9b1428cfd427d5d21564e69f6351583552ad1d9
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-23 18:55:37 +0100

    Update Linear_regression.R

--- Linear_regression.R
+++ Linear_regression.R
@@ -9,11 +9,26 @@ ex_y <- sum(y) / length(y)
 sq_ex_x <- sum(x^2) / length(x)
 ex_xy <- sum(x*y) / length(x)
 m <- (ex_x * ex_y - ex_xy) / ((ex_x)^2 - sq_ex_x)
-
+m <- round (m, digits = 4)
 # 2- calculate b 
-b <- ex_y - m * ex_x
+b <- round(ex_y - m * ex_x)
 
-#3- print the regression line:
-sprintf("y= %s + %s * x", b, m)
+#print the regression line:
+sprintf('y= %s + %s * x', b, m)
+
+# 3- calculte squared error 
+sq_err <- sum((y-m*x-b)^2)
+sprintf('The squared error is %s', sq_err)
+
+#plot data and regression line
+x1 <- 0:5
+plot(x,y, col="#0300FF")
+lines(x1, b+m*x1, type = "p", col ="red")
+
+#rewrite linear regression but with lm function
+li_fun <- lm(y ~ x)
+summary(li_fun)
+#plot regression line
+abline(li_fun)
 
 
