e5f7c5d026a5376a03b868c7555f4bdc3de2adc5
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-24 23:14:46 +0100

    Add files via upload

---
+++ KNN.R
@@ -0,0 +1,11 @@
+#creating data for KNN
+x <- c(7,7,3,1)
+y <- c(7,4,4,4)
+z <- c("Bad", "Bad", "Good", "Good")
+
+test_data <- c(3,7)
+
+#building KNN from scratch, by using euclidean distance
+
+
+
