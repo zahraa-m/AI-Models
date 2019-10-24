6ef8ba934cfdc3e1c28f353bbcc931ed701353a8
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-24 23:24:24 +0100

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -3,9 +3,13 @@ x <- c(7,7,3,1)
 y <- c(7,4,4,4)
 z <- c("Bad", "Bad", "Good", "Good")
 
-test_data <- c(3,7)
-
+td <- c(3,7)
+td[1]
 #building KNN from scratch, by using euclidean distance
+
+ds <- sqrt(((x-td[1])^2)+((y-td[2])^2))
+ds
+
 
 
 
