30f09302993cf4bf914bce487dfc81d479683a89
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-25 02:11:53 +0100

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -2,24 +2,17 @@
 x <- c(7,7,3,1)
 y <- c(7,4,4,4)
 z <- c("Bad", "Bad", "Good", "Good")
-
 da_se <- data.frame(type=x, color=y, taste=z)
-da_se
+
+#testing data point
 td <- c(3,7)
-td[1]
+
 #building KNN from scratch, by using euclidean distance
-
 ds <- sqrt(((x-td[1])^2)+((y-td[2])^2))
-ds
-ra <- order(ds)
-da_se <- data.frame(type=x, color=y, taste=z, distance=ra)
+ra <- rank(ds)
+da_se <- data.frame(type=x, color=y, taste=z, distance=ds, rank=ra)
 da_se
-
-if (ra==1){
-  print("the test data is Good")
-}else{
-  print("test data is Bad")
-}
-
+ta <-which.min(ra)
+sprintf("the test data is %s", z[ta])
 
 
