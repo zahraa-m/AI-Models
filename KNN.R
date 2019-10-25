6a84025fd2fb168cbc99098701a396096c81fbb6
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-25 02:14:16 +0100

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -1,11 +1,10 @@
-
 #creating data for KNN
 x <- c(7,7,3,1)
 y <- c(7,4,4,4)
 z <- c("Bad", "Bad", "Good", "Good")
 da_se <- data.frame(type=x, color=y, taste=z)
 
-#testing data point
+#new data point
 td <- c(3,7)
 
 #building KNN from scratch, by using euclidean distance
@@ -14,5 +13,5 @@ ra <- rank(ds)
 da_se <- data.frame(type=x, color=y, taste=z, distance=ds, rank=ra)
 da_se
 ta <-which.min(ra)
-sprintf("the test data  point classified as %s", z[ta])
+sprintf("the new data point classified as %s", z[ta])
 
