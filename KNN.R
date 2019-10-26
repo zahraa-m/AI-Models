b6a67156728b7e4812130ac1e29d2a806483ff51
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-26 23:47:25 +0100

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -14,3 +14,19 @@ da_se <- data.frame(type=x, color=y, taste=z, distance=ds, rank=ra)
 da_se
 ta <-which.min(ra)
 sprintf("the new data point classified as %s", z[ta])
+
+
+library(readr)
+da <- read_csv("Desktop/data.csv")
+da
+#as.table(da_se, keep.rownames=TRUE)
+train_d <-da[1:3, ]
+train_d
+test_d <- da[4, ]
+cl_d <- da[1:3, 3]
+cl_d
+#Rewrite KNN by using knn function
+require(class)
+pre <- knn(train = train_d, test=test_d, cl=cl_d, k=1)
+pre
+
