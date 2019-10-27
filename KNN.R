21909d6ba90b1fd113da76e263317278cd5b83e5
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-27 01:11:59 +0100

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -10,22 +10,7 @@ td <- c(3,7)
 #building KNN from scratch, by using euclidean distance
 ds <- sqrt(((x-td[1])^2)+((y-td[2])^2))
 ra <- rank(ds)
-da_se <- data.frame(type=x, color=y, taste=z, distance=ds, rank=ra)
-da_se
+data.frame(type=x, color=y, taste=z, distance=ds, rank=ra)
 ta <-which.min(ra)
 sprintf("the new data point classified as %s", z[ta])
-
-
-library(readr)
-da <- read_csv("Desktop/data.csv")
-da
-train_d <-da[1:3, ]
-train_d
-test_d <- da[4, ]
-cl_d <- da[1:3, 3]
-cl_d
-#Rewrite KNN by using knn function
-require(class)
-pre <- knn(train = train_d, test=test_d, cl=cl_d, k=1)
-pre
 
