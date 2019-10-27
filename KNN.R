4e584c8b78f27c2854dcaa82bc8c2a0d7f83f8cb
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-27 01:11:03 +0100

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -4,7 +4,7 @@ y <- c(7,4,4,4)
 z <- c("Bad", "Bad", "Good", "Good")
 da_se <- data.frame(type=x, color=y, taste=z)
 
-#new data point
+#new data point to test KNN
 td <- c(3,7)
 
 #building KNN from scratch, by using euclidean distance
