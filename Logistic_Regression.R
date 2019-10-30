560bca848a62bb2b238d6b9993d79f78be2da986
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-30 23:40:12 +0000

    Update Logistic_Regression.R

--- Logistic_Regression.R
+++ Logistic_Regression.R
@@ -3,3 +3,18 @@ da <- read_csv("Desktop/LR.csv")
 str(da)
 n <- da$Pass
 x <- da$Hours
+
+
+likelihood <- function(b0, b1){
+  p <- 1/(1+10^(-b0-b1*x))
+  if (n==0){
+    f <- 1-p
+    f-log <- log(f)
+    f-log
+  } else{
+    s-log <- log(p)
+    s-log
+  }
+  -sum(s-log+f-log)
+}
+
