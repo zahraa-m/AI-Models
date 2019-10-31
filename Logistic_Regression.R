abed53cd6aee68b8b362e1d31887ca97211db247
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-10-31 17:57:21 +0000

    Update Logistic_Regression.R

--- Logistic_Regression.R
+++ Logistic_Regression.R
@@ -1,21 +1,21 @@
-#load dataset LR
 library(readr)
 da <- read_csv("Desktop/LR.csv")
 str(da)
 n <- da$Pass
 x <- da$Hours
 
-#building mle to calculate Logistic regression parameters(coefficients), b0 and b1.
+f<-numeric()
+s<-numeric()
+
 likelihood <- function(b0, b1){
   p <- 1/(1+10^(-b0-b1*x))
-  if (n==0){
-    f <- 1-p
-    f-log <- log(f)
-    f-log
-  } else{
-    s-log <- log(p)
-    s-log
-  }
-  -sum(s-log+f-log)
-}
-
+  for (i in 1:20) {
+    if (n[i]==1){
+      f<- append(f,n[i])
+      
+    } else{
+      s <- append(s,n[i])
+      s
+    }
+    
+    
