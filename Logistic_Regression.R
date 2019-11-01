28ae5b6ac896eaabf80ad5a117c22259ff959a85
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-11-01 21:57:03 +0000

    Update Logistic_Regression.R

--- Logistic_Regression.R
+++ Logistic_Regression.R
@@ -8,24 +8,22 @@ f<-numeric()
 s<-numeric()
 
 likelihood <- function(b0, b1){
-  p <- 1/(1+10^(-b0-b1*x))
+  p <- 1/(1+10^-(b0+b1*x))
   for (i in 1:20) {
     if (n[i]==1){
       f<- append(f,p[i])
-      
     } else{
       s <- append(s,p[i])
-      s
     }
     
   }
-  f<-log(f)
-  s<-log(s)
-  -sum(f+s)
+  s <- 1-s
+  all_p<- c(s,f)
+  -sum(log(all_p))
 }
 
 #install.packages('stats4')
 library(stats4)
 library(methods)
-mle(minuslogl = likelihood, start = list(b0 = 0.2, b1 = 0.3), method = "L-BFGS-B")
+mle(minuslogl = likelihood, start = list(b0 = 0, b1 = 0), method = "BFGS")
 
