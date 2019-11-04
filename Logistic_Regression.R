22a0c2e2f94840e4f722007aea7513b1f5946fd9
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-11-04 23:17:46 +0000

    Update Logistic_Regression.R

--- Logistic_Regression.R
+++ Logistic_Regression.R
@@ -4,29 +4,27 @@ str(da)
 n <- da$Pass
 x <- da$Hours
 
-f<-numeric()
-s<-numeric()
+f<-numeric() #vector for fail, when Y(x)=0
+s<-numeric() #vector for success, when Y(x)=1
 
 likelihood <- function(b0, b1){
-  p <- 1/(1+10^-(b0+b1*x))
+  p <- 1/(1+10^(-b0-b1*x))
   for (i in 1:20) {
     if (n[i]==1){
-      f<- append(f,p[i])
+      s<- append(s,p[i])
     } else{
-      s <- append(s,p[i])
+      f <- append(f,p[i])
     }
     
   }
-  s <- 1-s
+  f <- 1-f
   all_p<- c(s,f)
   -sum(log(all_p))
 }
 
 #install stats4 package to call mle(maximum likelihood estimation) function
-install.packages('stats4')
+#install.packages('stats4')
 library(stats4)
 library(methods)
-#method = should be one of “Nelder-Mead”, “BFGS”, “CG”, “L-BFGS-B”, “SANN”, “Brent”
-#start= are just initial values
-mle(minuslogl = likelihood, start = list(b0 = 0, b1 = 0), method = "BFGS")
-
+lor <-mle(minuslogl = likelihood, start = list(b0 = 0, b1 = 0), method = "BFGS")
+summary(lor)
