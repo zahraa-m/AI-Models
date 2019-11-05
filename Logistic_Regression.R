4b8908b80b219874302528e341a4f91badc69604
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-11-05 22:42:42 +0000

    Update Logistic_Regression.R

--- Logistic_Regression.R
+++ Logistic_Regression.R
@@ -4,7 +4,7 @@ str(da)
 n <- da$Pass
 x <- da$Hours
 
-#plot(x,n, xlab = "Hours", ylab = "Pass")
+plot(x,n, xlab = "Hours", ylab = "Pass")
 
 f<-numeric() #vector for fail, when Y(x)=0
 s<-numeric() #vector for success, when Y(x)=1
@@ -33,4 +33,12 @@ library(methods)
 lor <-mle(minuslogl = likelihood, start = list(b0 = 0, b1 = 0), method = "BFGS")
 summary(lor)
 sprintf("The probability of passing the exam = 1 / 1 + exp (-(%s*X%s)) ", lor@coef[["b1"]], lor@coef[["b0"]])
+
+#plotting the logistic regression
+e <- 2.71828
+b0 <- lor@coef[["b0"]]
+b1 <- lor@coef[["b1"]]
+pro= 1/(1+e^(-b0-b1*x))
+pro
+plot(x, pro, xlab = "Hours", ylab = "The probability of passing the exam" )
 
